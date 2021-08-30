7821f6dd43aa6bc85c97b11eb6b791b4d8eb4a59
richycardo7 <famar414@gmail.com>
2021-08-29 22:12:08 -0600

    Form Control

--- src/app/menu/modal-menu/modal-menu.component.ts
+++ src/app/menu/modal-menu/modal-menu.component.ts
@@ -1,41 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ThrowStmt } from '@angular/compiler';
-import { MatDialogRef } from '@angular/material/dialog';
-import { DataService } from 'src/app/conection/data.service';
+import { Component, OnInit } from "@angular/core";
+import { ThrowStmt } from "@angular/compiler";
+import { MatDialogRef } from "@angular/material/dialog";
+import { DataService } from "src/app/conection/data.service";
 @Component({
-  selector: 'app-modal-menu',
-  templateUrl: './modal-menu.component.html',
-  styleUrls: ['./modal-menu.component.scss']
+  selector: "app-modal-menu",
+  templateUrl: "./modal-menu.component.html",
+  styleUrls: ["./modal-menu.component.scss"],
 })
 export class ModalMenuComponent implements OnInit {
-  
-  firstname:string ="";
-  lastname:string ="";
-  location:string ="";
-  cellphone:string ="";
+  firstname: string = "";
+  lastname: string = "";
+  location: string = "";
+  cellphone: string = "";
   gender: string = "";
-  constructor(public dialogRef: MatDialogRef<ModalMenuComponent>,private datasv:DataService) {      
+  el = document.getElementById("incorrectData");
+
+  constructor(
+    public dialogRef: MatDialogRef<ModalMenuComponent>,
+    private datasv: DataService
+  ) {}
+
+  celular: number = 0;
+  ngOnInit(): void {}
+
+  actionAgregar() {
+    if (this.checkData()) {
+      var d = new Date();
+      this.datasv
+        .adduser({
+          firstName: this.firstname,
+          lastName: this.lastname,
+          location: this.location,
+          phone: this.cellphone,
+          gender: this.gender,
+          createdAt: d,
+        })
+        .subscribe();
+      alert(
+        "usario " +
+          this.firstname +
+          " " +
+          this.lastname +
+          " agregado correctamente"
+      );
+      window.location.reload();
+    } else {
+      alert("Favor de revisar que sus datos estén completos");
+    }
+  }
+  checkData() {
+    if (
+      this.firstname != "" &&
+      this.lastname != "" &&
+      this.location != "" &&
+      this.cellphone != "" &&
+      this.gender != ""
+    ) {
+      return true;
+    }
+    return false;
   }
 
-  celular:number=0;
-  ngOnInit(): void {
-    
-  }  
 
-  actionAgregar() {    
-    var d = new Date();
-    this.datasv.adduser({firstName: this.firstname,
-      lastName: this.lastname,
-      location: this.location ,
-      phone: this.cellphone,
-      gender: this.gender,
-      createdAt: d      
-  }).subscribe();
-  }
-
-  // If the user clicks the cancel button a.k.a. the go back button, then\
-  // just close the modal
   closeModal() {
     this.dialogRef.close();
+    window.location.reload();
   }
 }
--- src/app/users/user/modal/modal.component.ts
+++ src/app/users/user/modal/modal.component.ts
@@ -1,40 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { data_user } from 'src/app/conection/ModelInterface';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {Inject} from '@angular/core';
-import { DataService } from 'src/app/conection/data.service';
+import { Component, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { data_user } from "src/app/conection/ModelInterface";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Inject } from "@angular/core";
+import { DataService } from "src/app/conection/data.service";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.scss"],
 })
 export class ModalComponent implements OnInit {
- 
-  
-  constructor(public dialogRef: MatDialogRef<ModalComponent>, private datasv:DataService,
-     @Inject(MAT_DIALOG_DATA) public data: data_user ) { }
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    private datasv: DataService,
+    @Inject(MAT_DIALOG_DATA) public data: data_user
+  ) {}
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 
   actionUpdate() {
-    var d = new Date();
-    this.datasv.updateUsers({id: this.data.id,
-      firstName: this.data.firstName,
-      lastName: this.data.lastName,
-      location: this.data.location ,
-      phone: this.data.phone,
-      gender: this.data.gender,
-      updatedAt: d      
-  }).subscribe();   
+    if (this.checkData()) {
+      var d = new Date();
+      this.datasv
+        .updateUsers({
+          id: this.data.id,
+          firstName: this.data.firstName,
+          lastName: this.data.lastName,
+          location: this.data.location,
+          phone: this.data.phone,
+          gender: this.data.gender,
+          updatedAt: d,
+        })
+        .subscribe();
+      alert(
+        "usario " +
+          this.data.firstName +
+          " " +
+          this.data.lastName +
+          " agregado correctamente"
+      );
+      window.location.reload();
+    } else {
+      alert("Favor de revisar que sus datos estén completos");
+    }
+  }
+  checkData() {
+    if (
+      this.data.firstName != "" &&
+      this.data.lastName != "" &&
+      this.data.location != "" &&
+      this.data.phone != "" &&
+      this.data.gender != ""
+    ) {
+      return true;
+    }
+    return false;
   }
 
-  
   closeModal() {
     this.dialogRef.close();
+    window.location.reload();
   }
-  
 }
